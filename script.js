486984232ee6339d2b7e3dc70c2d51195ab3ebf6
imbriane0 <142589941+imbriane0@users.noreply.github.com>
2025-08-15 08:01:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: ""Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",,
+        alternativas:[
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    }
+   
+    
